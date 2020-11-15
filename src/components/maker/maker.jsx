1a56296b37ad573c2e3168b452d65b7d8a805d66
positiveko <edie_ko@naver.com>
2020-11-15 22:22:23 +0900

    [Add] Editor 및 Preview 레이아웃 추가

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
@@ -11,16 +13,19 @@ const Maker = ({ authService }) => {
   };
 
   useEffect(() => {
-    authService.onAuthChange((user) => {
+    authService.onAuthChange(user => {
       if (!user) {
         history.push('/');
       }
     });
   });
-
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
+      <div className={styles.container}>
+        <Editor />
+        <Preview />
+      </div>
       <Footer />
     </section>
   );
