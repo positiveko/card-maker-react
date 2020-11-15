81dfe2d02fca36398879a290b2121a205a4015a9
positiveko <edie_ko@naver.com>
2020-11-15 16:48:26 +0900

    [Set] Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.module.css';
+
+function App() {
+  return <div className='App'>안녕하세요</div>;
+}
+
+export default App;
---
+++ src/service/firebase.js
@@ -0,0 +1,10 @@
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AYTH_DOMAIN,
+    databaseURL:process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
