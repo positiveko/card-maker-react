381102524b3704ea73827aaa55f3a76e3f4dee06
positiveko <edie_ko@naver.com>
2020-11-16 23:15:43 +0900

    [Add] editor, preview components 수정

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../footer/footer';
 import Header from '../header/header';
@@ -7,6 +7,42 @@ import Preview from '../preview/preview';
 import styles from './maker.module.css';
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: '1',
+      name: 'Edie',
+      company: 'Wecode',
+      theme: 'light',
+      title: 'Front-end Developer',
+      email: 'edie.spak1er@gamil.com',
+      message: 'Be positive',
+      fileName: 'edie',
+      fileURL: 'edie.png'
+    },
+    {
+      id: '2',
+      name: 'Edie2',
+      company: 'Wecode',
+      theme: 'light',
+      title: 'Front-end Developer',
+      email: 'edie.spak1er@gamil.com',
+      message: 'Be positive',
+      fileName: 'edie',
+      fileURL: 'edie.png'
+    },
+    {
+      id: '3',
+      name: 'Edie3',
+      company: 'Wecode',
+      theme: 'light',
+      title: 'Front-end Developer',
+      email: 'edie.spak1er@gamil.com',
+      message: 'Be positive',
+      fileName: 'edie',
+      fileURL: 'edie.png'
+    }
+
+  ])
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -23,8 +59,8 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor />
-        <Preview />
+        <Editor cards={cards}/>
+        <Preview cards={cards}/>
       </div>
       <Footer />
     </section>
